b93b540480f1e7141b4c77e0cd434e4c49fd4575
DrewYak <DrewYak7@gmail.com>
2022-05-26 23:29:46 +0300

    Добавлен задний фон

--- main.lua
+++ main.lua
@@ -3,12 +3,15 @@ push = require 'push'
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
-VIRTUAL_WIDTH = 512
-VIRTUAL_HEIGHT = 288
+VIRTUAL_WIDTH = 1280
+VIRTUAL_HEIGHT = 720
 
 local background = love.graphics.newImage('background.jpg')
 
+
 function love.load()
+	--love.graphics.setDefaultFilter('nearest', 'nearest')
+	
 	love.window.setTitle('Zombie Road 2D')
 
 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -20,4 +23,18 @@ end
 
 function love.resize(w, h)
 	push:resize(w, h)
+end
+
+function love.keypressed(key)
+	if key == 'escape' then
+		love.event.quit()
+	end
+end
+
+function love.draw()
+	push:start()
+
+	love.graphics.draw(background, 0, 0, 0, 0.5, 0.5)
+
+	push:finish()
 end
