e5d2995b8166a14fc9a92c9ab422718353f50958
DrewYak <DrewYak7@gmail.com>
2022-05-27 00:13:44 +0300

    Добавлен ручной тормоз, нажим и отжим педали газ

--- main.lua
+++ main.lua
@@ -11,10 +11,11 @@ local backgroundScroll = 0
 local backgroundScrollSpeed = 60
 local BACKGROUND_LOOPING_POINT = 512
 
+local speedUp = false
+local speedDown = false
+local handBreak = false
 
 function love.load()
-	--love.graphics.setDefaultFilter('nearest', 'nearest')
-	
 	love.window.setTitle('Zombie Road 2D')
 
 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -31,16 +32,36 @@ end
 function love.keypressed(key)
 	if key == 'escape' then
 		love.event.quit()
-	elseif key == 'd' then
-		backgroundScrollSpeed = backgroundScrollSpeed + 10
+	elseif key == 'space' then
+		handBreak = true
 	elseif key == 'a' then
-		backgroundScrollSpeed = backgroundScrollSpeed - 10
+		speedDown = true
+	elseif key == 'd' then
+		speedUp = true
+	end
+end
+
+function love.keyreleased(key, scancode)
+	if key == 'space' then
+		handBreak = false
+	elseif key == 'a' then
+		speedDown = false
+	elseif key == 'd' then
+		speedUp = false
 	end
 end
 
 function love.update(dt)
+	if handBreak then
+		backgroundScrollSpeed = math.max(0, backgroundScrollSpeed - 50)		
+	elseif speedDown then 
+		backgroundScrollSpeed = math.max(0, backgroundScrollSpeed - 15)
+	elseif speedUp then 
+		backgroundScrollSpeed = math.min(5000, backgroundScrollSpeed + 15)
+	end
+
 	backgroundScroll = (backgroundScroll + backgroundScrollSpeed * dt) 
-	% BACKGROUND_LOOPING_POINT
+	% BACKGROUND_LOOPING_POINT	
 end
 
 function love.draw()
